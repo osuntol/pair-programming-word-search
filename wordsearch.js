35c52799e58f56afa247fca31a30594f288152b3
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-01-18 21:59:41 +0000

    word search pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,43 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+    if (letters.length === 0) {
+        if (word === "") {
+            return true;
+        }
+        return false;
     }
+
+    
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    console.log("horizontalJoin", horizontalJoin);
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+
+    let verticalMatrix = [];
+
+    for (let i = 0; i < letters[0].length; i++) {
+        verticalMatrix.push([]);
+    }
+
+    
+
+    for (let col = 0; col < letters[0].length; col++) {
+        for (let row = 0; row < letters.length; row++) {
+            verticalMatrix[col].push(letters[row][col]);
+        }
+    }
+    
+    const verticalJoin = verticalMatrix.map(ls => ls.join(''));
+    console.log("verticalJoin", verticalJoin);
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 module.exports = wordSearch
